0bb8aca5b26beb150615a658fe0fc2d30f1f3237
garvinsolanki <94673012+garvinsolanki@users.noreply.github.com>
2022-01-04 18:04:19 +0530

    Add files via upload

--- TASK2.C
+++ TASK2.C
@@ -2,14 +2,23 @@
 #include<conio.h>
 void main()
 {
-	int a[10],i,n;
+	int a[10],i,n,max;
 	clrscr();
 	printf("enter array size=");
 	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+	for(i=0;i<n;i++)
 	{
 		printf("enter a[%d]=",i);
 		scanf("%d",&a[i]);
 	}
+	max=a[0];
+	for(i=0;i<n;i++)
+	{
+		if(max<a[i])
+		{
+			max=a[i];
+		}
+	}
+	printf("largest number=%d",max);
 	getch();
 }
